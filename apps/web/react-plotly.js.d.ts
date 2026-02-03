a076402e77c8ad17f21574c4900c19b9c0d162b0
Andrew Maguire <andrewm4894@gmail.com>
2026-02-03 19:51:51 +0000

    Fix react-plotly.js type declaration to export component

--- apps/web/react-plotly.js.d.ts
+++ apps/web/react-plotly.js.d.ts
@@ -1,2 +1,8 @@
-declare module "react-plotly.js";
+declare module "react-plotly.js" {
+  import { Component } from "react";
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const Plot: Component<any>;
+  export default Plot;
+}
+
 declare module "plotly.js-dist-min";
